2d1cdfc5f9d1c7b064e3b33e747eecb4166e9cd1
Manjit Bedi <dev@manjitbedi.com>
2019-08-20 14:07:19 -0700

    Initial Commit

---
+++ Face Detector/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Face Detector
+//
+//  Created by Manjit Bedi on 2019-08-20.
+//  Copyright © 2019 Noorg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Face Detector/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Face Detector
+//
+//  Created by Manjit Bedi on 2019-08-20.
+//  Copyright © 2019 Noorg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
